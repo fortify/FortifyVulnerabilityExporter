8abc19a3a1a523198776cad75e387e360c782493
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2023-01-28 13:50:07 +0100

    docs: Update documentation structure

--- doc-resources/update-repo-doc-resources.sh
+++ doc-resources/update-repo-doc-resources.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) source <(curl -s https://raw.githubusercontent.com/fortify/shared-doc-resources/main/scripts/update-doc-resources.sh)
+source <(curl -s https://raw.githubusercontent.com/fortify/shared-doc-resources/main/scripts/update-doc-resources.sh)
